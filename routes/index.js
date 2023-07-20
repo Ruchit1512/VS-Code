83ae89ff7cb2bee8cbc05b63299ddcf239e0d733
ruchit1512 <ruchitrahul44@gmail.com>
2023-07-20 21:26:49 +0530

    create a file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  fs.readdir('./Files',function(err,data){
+    res.render('index',{ title: 'Express',data:data});
+  })
 });
+
+
+router.post('/createfile', function(req, res) {
+  fs.writeFile(`./Files/${req.body.filename}`, "", function(err,data){
+   res.redirect('/')
+  })
+});
+
+
+router.post('/createfolder', function(req, res) {
+  fs.mkdir(`./Files/${req.body.foldername}`, function(err,data){
+   res.redirect('/')
+  })
+});
+
+
+
+
+
+
+router.get('/dlt/:naam', function(req,res,next){
+  fs.unlink(`./files/${req.params.naam}`,function(err,data){
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
