d2509f701b3b39ed084d6c2760d96f60e5797fa4
ruchit1512 <ruchitrahul44@gmail.com>
2023-07-22 13:57:44 +0530

    Almost done

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,37 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
+
 /* GET home page. */
+
 router.get('/', function (req, res) {
-  fs.readdir('./Files', { withFileTypes: true }, function (err, files) {
-    res.render('index', { files: files })
+  fs.readdir('./Files', { withFileTypes: true }, function (err, elem) {
+    res.render('index', { elem });
   })
 })
 
 
+router.get(`/delete/:filename`, function (req, res) {
+  fs.unlink(`./Files/${req.params.filename}`, function (err) {
+    res.redirect('/');
+  })
+})
+
+
+
 router.post('/createfile', function (req, res) {
   fs.writeFile(`./Files/${req.body.filename}`, "", function (err, data) {
-    res.redirect('/')
+    res.redirect('/');
   })
 });
+
 
 
 router.post('/createfolder', function (req, res) {
-  fs.mkdir(`./Files/${req.body.foldername}`,{recursive:true}, function (err, data) {
+  fs.mkdir(`./Files/${req.body.foldername}`, { recursive: true }, function (err, data) {
     res.redirect('/')
   })
 });
-
-
-
-
 
 
 module.exports = router;
