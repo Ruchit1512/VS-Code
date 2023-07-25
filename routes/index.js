ec1ab155a7732ef6b112c375af3d1b5661312639
ruchit1512 <ruchitrahul44@gmail.com>
2023-07-25 23:02:54 +0530

    New changes

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,56 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
-
-/* GET home page. */
-
-router.get('/', function (req, res) {
-  fs.readdir('./Files', { withFileTypes: true }, function (err, elem) {
-    res.render('index', { elem });
-  })
-})
-
-
-router.get(`/delete/:filename`, function (req, res) {
-  fs.unlink(`./Files/${req.params.filename}`, function (err) {
-    res.redirect('/');
-  })
-})
-
-
-
-router.post('/createfile', function (req, res) {
-  fs.writeFile(`./Files/${req.body.filename}`, "", function (err, data) {
-    res.redirect('/');
+router.get('/', function(req, res) {
+  fs.readdir(`./Files`, {withFileTypes: true}, function(err, elem){
+    res.render("index", {elem})
   })
 });
 
+router.get('/back', function(req, res) {
+  res.redirect("back")
+});
 
-
-router.post('/createfolder', function (req, res) {
-  fs.mkdir(`./Files/${req.body.foldername}`, { recursive: true }, function (err, data) {
-    res.redirect('/')
+router.get('/file/:filename', function(req, res) {
+  fs.readdir(`./Files`, {withFileTypes: true}, function(err, elem){
+    fs.readFile(`./Files/${req.params.filename}`, "utf8", function(err, filedata){
+      res.render("opened", {elem, filename: req.params.filename, filedata});
+    })
   })
 });
 
+router.post('/update/:filename', function(req, res) {
+  fs.writeFile(`./Files/${req.params.filename}`, req.body.data, function(err){
+    res.redirect("back");
+  })
+});
+
+router.get('/delete/:type/:filename', function(req, res) {
+  if(req.params.type === "folder"){
+    fs.rmdir(`./Files/${req.params.filename}`, function(err){
+      res.redirect("back");
+    })
+  }
+  else{
+    fs.unlink(`./Files/${req.params.filename}`, function(err){
+      res.redirect("back");
+    })
+  }
+});
+
+router.get('/createfile', function(req, res) {
+  fs.writeFile(`./Files/${req.query.filename}`, "", function(err){
+    if(err) res.send(err);
+    else res.redirect("back")
+  })
+});
+
+router.get('/createfolder', function(req, res) {
+  fs.mkdir(`./Files/${req.query.foldername}`, function(err){
+    if(err) return err;
+    else res.redirect("back");    
+  })
+});
 
 module.exports = router;
